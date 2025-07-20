c3efb32b66afc33588d23c5f0e86cbf2b713a90f
Peter-Truman <63771287+Peter-Truman@users.noreply.github.com>
2025-07-21 09:49:30 +1000

    Trying Grok

---
+++ XC8_PG_vB_200725.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XC8_PG_vB_200725.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XC8_PG_vB_200725.X.production.hex
